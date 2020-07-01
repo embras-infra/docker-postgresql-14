f4eb5b8ab04f2bf9ed3929f087c5b340157a7f3f
brunoalmeida <bruno_melo_almeida@hotmail.com>
2020-07-01 09:45:45 -0300

    Refatorado kill connection

--- files2docker/scripts/kill-connection.sh
+++ files2docker/scripts/kill-connection.sh
@@ -24,8 +24,5 @@ if [ -z $DB_NAME ]; then
   exit 1;
 fi
 
-## Mata todos os processos referente ao banco
-ps -aux | grep $DB_NAME | awk '{ print $2 }' | sudo xargs kill -9
-
 #### Mata as conexões com o banco
 psql -U $POSTGRES_USER -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$DB_NAME' AND pid <> pg_backend_pid();"
